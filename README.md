64632df5f6309e503985b6fb2c7133cdc2bb267f
davidot <david.t@glazenkamp.net>
2015-05-16 01:47:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# FunStats
---
+++ src/FunStats.java
@@ -0,0 +1,15 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * todo
+ *
+ * @author davidot
+ */
+public class FunStats extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+
+
+    }
+}
